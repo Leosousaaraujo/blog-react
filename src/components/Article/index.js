e616ab039d7495bfde4e6aa4d2f5408541da6584
Leosousaaraujo <theleon.tl@gmail.com>
2022-01-22 17:51:19 -0300

    Estilização finalizada

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -2,10 +2,33 @@ import Post from '../Post';
 
 function Article(){
     return(
-        <>
+        <section className="container">
             <h1>Artigo</h1>
-            <Post></Post>
-        </>
+            <div className="mt-5 container-posts">
+            <Post subtitulo="Tecnologia" titulo="O guia definitivo do blog">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+            tincidunt mauris urna, non elementum magna porttitor sit amet. 
+            Curabitur at convallis eros. Nulla pellentesque sem ac ipsum auctor 
+            vulputate. Donec ex ipsum, tempor quis felis non, sollicitudin 
+            tincidunt dui. Suspendisse potenti. Morbi pellentesque eros quis gravida porttitor. 
+            </Post>
+            <Post subtitulo="Fotografia" titulo="Melhores cameras para 2021">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+            tincidunt mauris urna, non elementum magna porttitor sit amet. 
+            Curabitur at convallis eros. Nulla pellentesque sem ac ipsum auctor 
+            vulputate. Donec ex ipsum, tempor quis felis non, sollicitudin 
+            tincidunt dui. Suspendisse potenti. Morbi pellentesque eros quis gravida porttitor.
+            </Post>
+            <Post subtitulo="Cinema" titulo="Os 10 filmes com maior bilheteria">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer 
+            tincidunt mauris urna, non elementum magna porttitor sit amet. 
+            Curabitur at convallis eros. Nulla pellentesque sem ac ipsum auctor 
+            vulputate. Donec ex ipsum, tempor quis felis non, sollicitudin 
+            tincidunt dui. Suspendisse potenti. Morbi pellentesque eros quis gravida porttitor.
+            </Post>
+            </div>
+
+        </section>
     );
 }
 
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,6 +1,20 @@
-const Post = () =>{
+const Post = (props) =>{
     return(
-        <h1>Post</h1>
+        <div className="post mx-2">
+            <div className="img-post"></div>
+            <h5>{props.subtitulo}</h5>
+            <h3>{props.titulo}</h3>
+            <p className="mt-1">
+            {props.children}
+            </p>
+            <div className="flex pt-2">
+                <div className="img-profile"></div>
+                <div className="desc-profile ml-2">
+                    <h6 className="color-blue">Fulano de tal</h6>
+                    <p>Aug 2, 2020 - 8 min read</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
