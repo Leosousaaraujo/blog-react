550512a169681d22fe56eb3993dcd0da228f10e2
Leosousaaraujo <theleon.tl@gmail.com>
2022-01-22 17:05:26 -0300

    Estilizando o Menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,24 @@
+import logo from '../../svg/logo.svg';
+
+
 export default function Header(){
+    const descLogo = "Essa é a logo do blog.";
+
     return (
-        <h1>Header.js</h1>
+        <>
+            <header className="flex-space-between">
+                <div className="logo">
+                    <img src={logo} alt={descLogo}/>
+                </div>
+                <div className="search">
+                    <input tyle="text" name="search" className="input-search"/>
+                </div>
+                <ul className="menu">
+                    <li><a href="#" className="nav-link">Categorias</a> </li>
+                    <li><a href="#" className="nav-link">Sobre</a> </li>
+                    <li><a href="#" className="nav-link">Contato</a> </li>
+                </ul>
+            </header>
+        </>
     );
 }
